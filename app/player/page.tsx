e8a5a9f5cc7f2a3e79a4046a23a9686fa8619d96
lemajr <erickbale360@gmail.com>
2025-01-24 22:49:18 +0300

    fix typo

--- app/player/page.tsx
+++ app/player/page.tsx
@@ -20,7 +20,7 @@ export default function Player() {
           const results = await fetchMusicByGenre(genre);
           setTracks(results);
         }
-      } catch (error) {
+      } catch (err: any) {
         setError('Failed to fetch music tracks.');
       } finally {
         setLoading(false);
